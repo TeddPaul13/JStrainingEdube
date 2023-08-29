6bc67e7952e2c6a7f511cb65f4ae5212c1b4e331
TeddPaul13 <teddtycoon@gmail.com>
2023-08-29 22:01:00 +1000

    done module 3 lab

---
+++ JavaScript-Essentials-1/Module3/app3.js
@@ -0,0 +1,45 @@
+//Let's go back to our contact list. After some recent tweaks (i.e. using an array and objects) it is actually starting to look like a list. 
+//However, one quite serious problem remained. 
+//Changing data in the list, such as adding a new contact, has to be provided for right away in the program code
+//What if we want to give the user the ability to enter the data of the added contact while the program is running? 
+//Modify the program to add, at the end of the list, not the contact, which is given in the code, but the one which the user will give during the program run. 
+//Use the prompt method to do this. At the end, display the first and the last contact from the list.
+
+
+
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    }, 
+    {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+    }, 
+    {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+    }];
+    
+    // write your code here
+    let newContact = {};
+    let addName = "Teddy" //window.prompt("Enter your name")
+    newContact.name = addName ? addName : "Test";
+
+    let addPhone = "0422 343 343" //window.prompt("Enter your phone number")
+    newContact.phone = addPhone ? addPhone : 1111;
+
+    let addEmail = "scot@email.com"; ////window.prompt("Enter your email")
+    newContact.email = addEmail ? addEmail : "test@email.com"
+   
+    console.log (newContact);
+
+    contacts.push(newContact)
+    
+    let last = contacts.length - 1;
+    
+    console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+    console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+    
